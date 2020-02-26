7afee28bc62a3abeedb96ce2db2ba83847d496b2
Kshitij Gupta <kshitijgm@gmail.com>
2020-02-26 06:58:38 +0000

    begonia: Add vendor symlinks
    Change-Id: I22f214048014cd0085a75980b0696aea1b3df92b
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,31 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),begonia)
+
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := vendor_symlinks
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE_SUFFIX := -timestamp
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/Android.mk
+$(LOCAL_BUILT_MODULE):
+	$(hide) echo "Making vendor symlinks"
+	@ln -sf /mnt/vendor/cust $(TARGET_OUT_VENDOR)/cust
+	@ln -sf /mnt/vendor/nvcfg $(TARGET_OUT_VENDOR)/nvcfg
+	@ln -sf /mnt/vendor/nvdata $(TARGET_OUT_VENDOR)/nvdata
+	@ln -sf /mnt/vendor/persist $(TARGET_OUT_VENDOR)/persist
+	@ln -sf /mnt/vendor/protect_f $(TARGET_OUT_VENDOR)/protect_f
+	@ln -sf /mnt/vendor/protect_s $(TARGET_OUT_VENDOR)/protect_s
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib64/hw
+	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
+	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
+	$(hide) touch $@
+
 endif
